2946f8f42d9b186f22fcbc3d1c3047592d28c2b2
Ninuskaninus <ninjaa181@gmail.com>
2023-06-01 22:41:28 +0200

    Added files

---
+++ script.js
@@ -0,0 +1,10 @@
+const button = document.querySelector('button');
+const screenHeight = window.innerHeight;
+button.addEventListener('click', () => {
+    scrollTo({
+        top: screenHeight * 1,
+        left: 0,
+        behavior: 'smooth'
+
+    });
+});
